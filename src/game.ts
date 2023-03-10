19f8c572e31796d6b536b6bd166a4cd5a73491e8
Ingaaa <106094638+ShinyDCL@users.noreply.github.com>
2023-03-10 20:44:36 +0200

    feat: add Twitter and Github links

--- src/game.ts
+++ src/game.ts
@@ -1,12 +1,5 @@
-import { getUserData, UserData } from '@decentraland/Identity';
-import { InstructionsLabel } from './ui/instructionsLabel';
-import {
-  addAnimation,
-  attachToAvatar,
-  createEntity,
-  createShape,
-  detachFromAvatar,
-} from './utils';
+import { setUpWands } from './setUpWands';
+import { createEntity, createShape } from './utils';
 
 // Grass
 createEntity(
@@ -29,7 +22,6 @@ const wand1 = createEntity(
   },
   wandShape
 );
-const wand1Transform = wand1.getComponent(Transform);
 const wand2 = createEntity(
   {
     position: new Vector3(57.6, 1.27, 54),
@@ -37,72 +29,32 @@ const wand2 = createEntity(
   },
   wandShape
 );
-const wand2Transform = wand2.getComponent(Transform);
 
-// Transform object for attaching wand to avatar
-const attachTransform = new Transform({
-  position: new Vector3(0.4, -0.7, 0.5),
-  rotation: Quaternion.Euler(45, 180, 0),
-});
-
-// Get user data
-let userData: UserData | null;
-void executeTask(async () => {
-  userData = await getUserData();
-});
-
-// Set up animations
-const vaseExplosion = addAnimation('Animation', store);
-addAnimation('WandLight', wand1);
-const light = addAnimation('WandLight', wand2);
-
-// Set up instructions label
-const canvas = new UICanvas();
-const texture = new Texture('images/background.png');
-const instructionsLabel = new InstructionsLabel(
-  canvas,
-  texture,
-  `Press 'E' to try it out, 'F' to put it down`
+// Github link
+const githubLink = createEntity(
+  { position: new Vector3(62, 1.3, 65) },
+  createShape('models/default/githubLink.glb')
+);
+githubLink.addComponent(
+  new OnPointerDown(
+    () => openExternalURL('https://github.com/ShinyDCL/wand-store'),
+    { hoverText: 'Github' }
+  )
 );
 
-let currentWand: Entity | null = null;
-const input = Input.instance;
+// Twitter link
+const twitterLink = createEntity(
+  { position: new Vector3(61, 1.3, 65) },
+  createShape('models/default/twitterLink.glb')
+);
+twitterLink.addComponent(
+  new OnPointerDown(
+    () =>
+      openExternalURL(
+        'https://twitter.com/ShinyDCL/status/1598760892962721792'
+      ),
+    { hoverText: 'Twitter' }
+  )
+);
 
-// Subscribe to event when button E is pressed
-input.subscribe('BUTTON_DOWN', ActionButton.PRIMARY, false, () => {
-  if (!currentWand) return;
-  if (currentWand.uuid === wand1.uuid) vaseExplosion.play();
-  if (currentWand.uuid === wand2.uuid) light.play();
-});
-
-// Subscribe to event when button F is pressed
-input.subscribe('BUTTON_DOWN', ActionButton.SECONDARY, false, () => {
-  if (!currentWand) return;
-  if (currentWand.uuid === wand1.uuid) {
-    vaseExplosion.stop();
-    detachFromAvatar(wand1, wand1Transform);
-  }
-  if (currentWand.uuid === wand2.uuid) {
-    light.stop();
-    detachFromAvatar(wand2, wand2Transform);
-  }
-
-  instructionsLabel.hide();
-  currentWand = null;
-});
-
-// Add interactions to wands
-const addOnPointerDown = (wand: Entity) => {
-  const onPointerDown = new OnPointerDown(
-    () => {
-      if (currentWand) return;
-      currentWand = wand;
-      attachToAvatar(wand, attachTransform, userData);
-      instructionsLabel.show();
-    },
-    { hoverText: 'Pick up!', button: ActionButton.POINTER }
-  );
-  wand.addComponentOrReplace(onPointerDown);
-};
-addOnPointerDown(wand1);
-addOnPointerDown(wand2);
+setUpWands(store, wand1, wand2);
