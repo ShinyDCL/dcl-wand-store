4793c1c63ee473f33b1448a99c84fa22aefcc5ba
Ingaaa <106094638+ShinyDCL@users.noreply.github.com>
2023-09-23 12:44:04 +0300

    feat: add skybox

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,36 @@
 import { Animator, engine, GltfContainer, InputAction, inputSystem, PointerEventType, Transform } from '@dcl/sdk/ecs'
 import { Quaternion, Vector3 } from '@dcl/sdk/math'
-import { SCENE_MIDDLE } from './config'
+import { SCENE_MIDDLE, SCENE_SIZE } from './config'
 import { Wand } from './wand'
 import { hideLabel, setupUI, showLabel } from './ui'
 import { setupLinks } from './links'
+import { setUpSkyBox } from './skyBox'
 
 export function main() {
   const scene = engine.addEntity()
   Transform.create(scene, { position: Vector3.create(SCENE_MIDDLE, 0, SCENE_MIDDLE) })
   GltfContainer.create(scene, { src: 'models/wandStore.glb' })
+
+  const border = engine.addEntity()
+  Transform.create(border, { parent: scene })
+  GltfContainer.create(border, { src: 'models/border.glb' })
+
+  const starModelSize = 8
+  const starModelMiddle = starModelSize / 2
+  const starModelCount = SCENE_SIZE / starModelSize
+  const rotations = [0, 90, 180, 270]
+
+  for (let i = 0; i < starModelCount; i++) {
+    for (let j = 0; j < starModelCount; j++) {
+      const randomYRotation = rotations[Math.floor(Math.random() * rotations.length)]
+      const stars = engine.addEntity()
+      Transform.create(stars, {
+        position: Vector3.create(starModelSize * i + starModelMiddle, 0, starModelSize * j + starModelMiddle),
+        rotation: Quaternion.fromEulerDegrees(0, randomYRotation, 0)
+      })
+      GltfContainer.create(stars, { src: 'models/stars.glb' })
+    }
+  }
 
   Animator.create(scene, {
     states: [
@@ -78,4 +100,5 @@ export function main() {
 
   setupUI()
   setupLinks(scene)
+  setUpSkyBox(scene)
 }
---
+++ src/skyBox.ts
@@ -0,0 +1,67 @@
+import { Entity, engine, Transform, MeshRenderer, Material } from '@dcl/sdk/ecs'
+import { Vector3, Quaternion } from '@dcl/sdk/math'
+import { SCENE_MIDDLE, SCENE_SIZE } from './config'
+
+export const setUpSkyBox = (parent: Entity): Entity => {
+  const folderPath = 'images/skybox'
+  const defaultScale = Vector3.create(SCENE_SIZE, SCENE_MIDDLE, SCENE_SIZE)
+
+  const skyBoxRoot = engine.addEntity()
+  Transform.create(skyBoxRoot, { position: Vector3.create(0, SCENE_MIDDLE / 2, 0), parent })
+
+  // Front
+  const skyBoxPZ = engine.addEntity()
+  Transform.create(skyBoxPZ, {
+    position: Vector3.create(0, 0, SCENE_MIDDLE),
+    scale: defaultScale,
+    parent: skyBoxRoot
+  })
+  MeshRenderer.setPlane(skyBoxPZ)
+  Material.setBasicMaterial(skyBoxPZ, { texture: Material.Texture.Common({ src: `${folderPath}/pz.jpg` }) })
+
+  // Back
+  const skyBoxNZ = engine.addEntity()
+  Transform.create(skyBoxNZ, {
+    position: Vector3.create(0, 0, -SCENE_MIDDLE),
+    rotation: Quaternion.fromEulerDegrees(0, 180, 0),
+    scale: defaultScale,
+    parent: skyBoxRoot
+  })
+  MeshRenderer.setPlane(skyBoxNZ)
+  Material.setBasicMaterial(skyBoxNZ, { texture: Material.Texture.Common({ src: `${folderPath}/nz.jpg` }) })
+
+  // Right
+  const skyBoxPX = engine.addEntity()
+  Transform.create(skyBoxPX, {
+    position: Vector3.create(SCENE_MIDDLE, 0, 0),
+    rotation: Quaternion.fromEulerDegrees(0, 90, 0),
+    scale: defaultScale,
+    parent: skyBoxRoot
+  })
+  MeshRenderer.setPlane(skyBoxPX)
+  Material.setBasicMaterial(skyBoxPX, { texture: Material.Texture.Common({ src: `${folderPath}/px.jpg` }) })
+
+  // Left
+  const skyBoxNX = engine.addEntity()
+  Transform.create(skyBoxNX, {
+    position: Vector3.create(-SCENE_MIDDLE, 0, 0),
+    rotation: Quaternion.fromEulerDegrees(0, -90, 0),
+    scale: defaultScale,
+    parent: skyBoxRoot
+  })
+  MeshRenderer.setPlane(skyBoxNX)
+  Material.setBasicMaterial(skyBoxNX, { texture: Material.Texture.Common({ src: `${folderPath}/nx.jpg` }) })
+
+  // Top
+  const skyBoxPY = engine.addEntity()
+  Transform.create(skyBoxPY, {
+    position: Vector3.create(0, SCENE_MIDDLE / 2, 0),
+    rotation: Quaternion.fromEulerDegrees(-90, 0, 0),
+    scale: Vector3.create(SCENE_SIZE, SCENE_SIZE, SCENE_SIZE),
+    parent: skyBoxRoot
+  })
+  MeshRenderer.setPlane(skyBoxPY)
+  Material.setBasicMaterial(skyBoxPY, { texture: Material.Texture.Common({ src: `${folderPath}/py.jpg` }) })
+
+  return skyBoxRoot
+}
